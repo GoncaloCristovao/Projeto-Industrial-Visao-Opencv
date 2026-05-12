a84324eac2c9cae585675fc3d55965cb6aca9152
GCristovao <goncalocristovao@ua.pt>
2026-05-12 22:46:48 +0100

    a

--- vision.py
+++ vision.py
@@ -94,9 +94,9 @@ class VisionProcessor:
                 if perfis_padrao and len(perfis_padrao) == len(dados_zonas["zonas"]):
                     for i, zona_atual in enumerate(dados_zonas["zonas"]):
                         zona_padrao = perfis_padrao[i]
-                        zona_atual["xp_cie"] = zona_padrao.get("x_cie", 0.333)
-                        zona_atual["yp_cie"] = zona_padrao.get("y_cie", 0.333)
-                        zona_atual["lump_padrao"] = zona_padrao.get("brilho_medio", 200.0)
+                        zona_atual["xp_cie"] = zona_padrao.get("x_cie", 0.333)  
+                        zona_atual["yp_cie"] = zona_padrao.get("y_cie", 0.333)  
+                        zona_atual["lump_padrao"] = zona_padrao.get("brilho_medio", 200.0)  
 
             # Empacota os dados para o Main enviar ao PLC e à HMI
             dados_saida = {
