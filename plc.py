b6c5bd6142c78fec5c6862d786a50f463f907aea
Alexandre Vieira <alexvieira1008@gmail.com>
2026-05-14 16:28:03 +0100

    Boas

--- plc.py
+++ plc.py
@@ -1,7 +1,7 @@
 import socket
 
 class PLCInterface:
-    def __init__(self, ip_plc="192.168.1.100", porta_plc=5000):
+    def __init__(self, ip_plc="192.168.54.251", porta_plc=5000):
         self.ip = ip_plc
         self.port = porta_plc
 
