9f9fde071b544fce7eef7efef0278e149bb1c61e
GCristovao <goncalocristovao@ua.pt>
2026-05-20 13:57:21 +0100

    das

--- plc.py
+++ plc.py
@@ -4,6 +4,18 @@ class PLCInterface:
     def __init__(self, ip_plc="192.168.54.251", porta_plc=5000):
         self.ip = ip_plc
         self.port = porta_plc
+        self.testar_conexao() # Faz o teste logo ao arrancar!
+
+    def testar_conexao(self):
+        """Tenta dar um 'toque' na porta do PLC só para ver se ele está lá."""
+        print(f"[PLC] A procurar o PLC no IP {self.ip}:{self.port}...")
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.settimeout(2.0) # Espera no máximo 2 segundos
+                s.connect((self.ip, self.port))
+            print("[PLC] LIGAÇÃO ESTABELECIDA COM SUCESSO! ")
+        except Exception as e:
+            print(f"[Erro PLC] AVISO: PLC não encontrado na rede! Verifique o IP ou o cabo. ({e})")
 
     def avaliar_peca_no_plc(self, id_padrao, zonas):
         try:
